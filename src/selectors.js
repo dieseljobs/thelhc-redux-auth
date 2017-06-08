b3f268487134ed7027e94eae502649b48f14c2f9
Aaron Kaczmarek <aaronmichaelmusic@gmail.com>
2017-06-08 11:13:40 -0400

    add isSpoof selector

--- src/selectors.js
+++ src/selectors.js
@@ -1,13 +1,42 @@
 import { createSelector } from 'reselect'
 
+/**
+ * Get the absolute signed-in user disregarding spoofUser
+ *
+ * @param  {Object} state
+ * @return {Object}
+ */
 export const absoluteUser = ( state ) => {
   return state.auth.user
 }
 
+/**
+ * Get the signed-in user ( will prioritize spoofUser )
+ *
+ * @param  {Object} state
+ * @return {Object}
+ */
 export const user = ( state ) => {
-  return state.auth.spoofUser ? state.auth.spoofUser : state.auth.user
+  return state.auth.spoofUser || state.auth.user || null
 }
 
+/**
+ * Determine if user is spoof
+ *
+ * @param  {Object}  state
+ * @return {Boolean}       
+ */
+export const isSpoof = ( state ) => {
+  return state.auth.spoofUser ? true : false
+}
+
+/**
+ * Determine if user has administrator privileges
+ * Accepts custom function block to resolve conditions
+ *
+ * @param  {Function} isAdminBlock
+ * @return {Boolean}
+ */
 export const isAdmin = ( isAdminBlock ) => {
   return createSelector(
     absoluteUser,
