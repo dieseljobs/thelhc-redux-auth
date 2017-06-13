5a5a80d0fd9a239ec20287c9486728accd555657
Aaron Kaczmarek <aaronmichaelmusic@gmail.com>
2017-06-13 15:19:21 -0400

    finish actions tests

--- test/actions.js
+++ test/actions.js
@@ -3,17 +3,26 @@ import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
-import { setUserAuth, checkForUser, resetAuth } from '../src/actions'
+import { setUserAuth, checkForUser, resetAuth, spoofUser } from '../src/actions'
 //import createReducer from '../src/reducer'
 
 describe('thunk actions', () => {
 
-
+  const userFixture = {
+    id: 1,
+    name: 'Aaron Kaz',
+    token: 'storedUserTokenabc123def456'
+  }
+  const spoofUserFixture = {
+    id: 2,
+    name: 'Bobby Brown',
+    token: 'storedSpoofUserToken'
+  }
   const middlewares = [ thunk ]
 
   let store;
   beforeEach(() => {
-    store = configureMockStore(middlewares)()
+    store = configureMockStore(middlewares)({auth: {}})
   })
 
   afterEach(() => {
@@ -22,21 +31,15 @@ describe('thunk actions', () => {
   })
 
   it('should handle setUserAuth', () => {
-    const user = {
-      id: 1,
-      fname: 'Aaron',
-      lname: 'Kaz',
-      token: 'abc123def456'
-    }
-    store.dispatch( setUserAuth( user ) )
+    store.dispatch( setUserAuth( userFixture ) )
     expect( store.getActions() ).toEqual([
       {
         type: '@@auth/SET_TOKEN',
-        token: 'abc123def456'
+        token: 'storedUserTokenabc123def456'
       },
       {
         type: '@@auth/SET_USER',
-        user: user
+        user: userFixture
       },
       {
         type: '@@auth/SET_AUTHENTICATED',
@@ -46,13 +49,8 @@ describe('thunk actions', () => {
   })
 
   it('should handle checkForUser with token in storage and user in session', () => {
-    const user= {
-      id: 1,
-      name: 'Aaron Kaz',
-      token: 'storedUserTokenabc123def456'
-    }
     localStorage.setItem( '@@auth/userToken', 'localTokenabc123def456' )
-    sessionStorage.setItem( '@@auth/user', JSON.stringify(user) )
+    sessionStorage.setItem( '@@auth/user', JSON.stringify(userFixture) )
 
     store.dispatch( checkForUser( axios, '/auth/token', '/api/account/user' ) )
       .then( () => {
@@ -67,7 +65,7 @@ describe('thunk actions', () => {
           },
           {
             type: '@@auth/SET_USER',
-            user: user
+            user: userFixture
           },
           {
             type: '@@auth/SET_AUTHENTICATED',
@@ -84,13 +82,8 @@ describe('thunk actions', () => {
 
   it('should handle checkForUser with token in storage and no user in session', () => {
     localStorage.setItem( '@@auth/userToken', 'localTokenabc123def456' )
-    const user = {
-      id: 1,
-      name: 'Aaron Kaz',
-      token: 'storedUserTokenabc123def456'
-    }
     const mock = new MockAdapter( axios )
-    mock.onGet('/api/account/user').reply(200, user)
+    mock.onGet('/api/account/user').reply(200, userFixture)
 
     store.dispatch( checkForUser( axios, '/auth/token', '/api/account/user' ) )
       .then( () => {
@@ -105,7 +98,7 @@ describe('thunk actions', () => {
           },
           {
             type: '@@auth/SET_USER',
-            user: user
+            user: userFixture
           },
           {
             type: '@@auth/SET_AUTHENTICATED',
@@ -121,14 +114,9 @@ describe('thunk actions', () => {
   })
 
   it('should handle checkForUser with no token in storage, no user in session, but has backend session', () => {
-    const user = {
-      id: 1,
-      name: 'Aaron Kaz',
-      token: 'storedUserTokenabc123def456'
-    }
     const mock = new MockAdapter( axios )
     mock.onGet('/auth/token').reply(200, {token: 'tokenFromAPIRequest'})
-    mock.onGet('/api/account/user').reply(200, user)
+    mock.onGet('/api/account/user').reply(200, userFixture)
 
     store.dispatch( checkForUser( axios, '/auth/token', '/api/account/user' ) )
       .then( () => {
@@ -143,7 +131,7 @@ describe('thunk actions', () => {
           },
           {
             type: '@@auth/SET_USER',
-            user: user
+            user: userFixture
           },
           {
             type: '@@auth/SET_AUTHENTICATED',
@@ -159,14 +147,8 @@ describe('thunk actions', () => {
   })
 
   it('should handle checkForUser with no token in storage, no user in session, and no backend session', () => {
-    const user = {
-      id: 1,
-      name: 'Aaron Kaz',
-      token: 'storedUserTokenabc123def456'
-    }
     const mock = new MockAdapter( axios )
     mock.onGet('/auth/token').reply(200, null)
-    mock.onGet('/api/account/user').reply(200, user)
 
     store.dispatch( checkForUser( axios, '/auth/token', '/api/account/user' ) )
       .then( () => {
@@ -185,19 +167,9 @@ describe('thunk actions', () => {
   })
 
   it('should handle checkForUser with token in storage, user in session, and spoof user', () => {
-    const user = {
-      id: 1,
-      name: 'Aaron Kaz',
-      token: 'storedUserTokenabc123def456'
-    }
-    const spoofUser = {
-      id: 2,
-      name: 'Bobby Brown',
-      token: 'storedSpoofUserToken'
-    }
     localStorage.setItem( '@@auth/userToken', 'localTokenabc123def456' )
-    sessionStorage.setItem( '@@auth/user', JSON.stringify(user) )
-    sessionStorage.setItem( '@@auth/spoofUser', JSON.stringify(spoofUser) )
+    sessionStorage.setItem( '@@auth/user', JSON.stringify(userFixture) )
+    sessionStorage.setItem( '@@auth/spoofUser', JSON.stringify(spoofUserFixture) )
 
     store.dispatch( checkForUser( axios, '/auth/token', '/api/account/user' ) )
       .then( () => {
@@ -208,7 +180,7 @@ describe('thunk actions', () => {
           },
           {
             type: '@@auth/SET_SPOOF_USER',
-            user: spoofUser
+            user: spoofUserFixture
           },
           {
             type: '@@auth/SET_TOKEN',
@@ -216,7 +188,7 @@ describe('thunk actions', () => {
           },
           {
             type: '@@auth/SET_USER',
-            user: user
+            user: userFixture
           },
           {
             type: '@@auth/SET_AUTHENTICATED',
@@ -225,6 +197,83 @@ describe('thunk actions', () => {
           {
             type: '@@auth/SET_IS_CHECKING',
             val: false
+          }
+        ])
+      })
+      .catch( err => console.log(err))
+  })
+
+  it('should handle resetAuth without spoof user', () => {
+    store.dispatch( resetAuth() )
+    expect( store.getActions() ).toEqual([
+      {
+        type: '@@auth/SET_AUTHENTICATED',
+        val: false
+      },
+      {
+        type: '@@auth/SET_USER',
+        user: {}
+      },
+      {
+        type: '@@auth/SET_TOKEN',
+        token: ''
+      },
+    ])
+  })
+
+  it('should handle resetAuth with spoof user', () => {
+    store = configureMockStore(middlewares)({
+      auth: {
+        spoofUser: {
+          name: 'Bobby Brown'
+        }
+      }
+    })
+    store.dispatch( resetAuth() )
+    expect( store.getActions() ).toEqual([
+      {
+        type: '@@auth/SET_SPOOF_USER',
+        user: ''
+      },
+    ])
+  })
+
+  it('should handle resetAuth with expire flag', () => {
+    store.dispatch( resetAuth( true ) )
+    expect( store.getActions() ).toEqual([
+      {
+        type: '@@auth/SET_AUTHENTICATED',
+        val: false
+      },
+      {
+        type: '@@auth/SET_USER',
+        user: {}
+      },
+      {
+        type: '@@auth/SET_TOKEN',
+        token: ''
+      },
+      {
+        type: '@@auth/EXPIRE_FLAG',
+        val: true
+      },
+      {
+        type: '@@auth/EXPIRE_FLAG',
+        val: false
+      },
+    ])
+  })
+
+  it('should handle spoofUser', () => {
+    const mock = new MockAdapter( axios )
+    mock.onPost('/auth/spoof-user').reply(200, spoofUserFixture)
+
+    store.dispatch( spoofUser( axios, '/auth/spoof-user', { id: 2 } ) )
+      .then( () => {
+        expect( store.getActions() ).toEqual([
+          {
+            type: '@@auth/SET_SPOOF_USER',
+            user: spoofUserFixture
           }
         ])
       })
