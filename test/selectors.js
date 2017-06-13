992f873539fb4541df22a622de9f2a53329bcd7a
Aaron Kaczmarek <aaronmichaelmusic@gmail.com>
2017-06-13 15:33:50 -0400

    test selectors

---
+++ test/selectors.js
@@ -0,0 +1,55 @@
+import expect from 'expect'
+import { selectAbsoluteUser, selectUser, isSpoof, createIsAdminSelector } from '../src/selectors'
+
+describe('selectors', () => {
+  let state = {
+    auth: {
+      isAuthenticated: true,
+      isChecking: false,
+      token: 'storedUserTokenabc123def456',
+      expireFlag: false,
+      user: {
+        id: 1,
+        name: 'Aaron Kaz',
+        token: 'storedUserTokenabc123def456',
+        level: 1
+      },
+      spoofUser: {
+        id: 2,
+        name: 'Bobby Brown',
+        token: 'storedSpoofUserToken'
+      }
+    }
+  }
+
+  it('should select absolute user', () => {
+    expect( selectAbsoluteUser( state ) ).toEqual( state.auth.user)
+  })
+
+  it('should select user (with spoof)', () => {
+    expect( selectUser( state ) ).toEqual( state.auth.spoofUser)
+  })
+
+  it('should resolve is spoof when spoof user present', () => {
+    expect( isSpoof( state ) ).toEqual( true )
+  })
+
+  it('should select user (without spoof)', () => {
+    delete state.auth.spoofUser
+    expect( selectUser( state ) ).toEqual( state.auth.user)
+  })
+
+  it('should resolve is spoof when spoof user not present', () => {
+    expect( isSpoof( state ) ).toEqual( false )
+  })
+
+  it('should handle createIsAdminSelector when true', () => {
+    expect( createIsAdminSelector( user => user.level === 1)( state ) ).toEqual( true )
+  })
+
+  it('should handle createIsAdminSelector when false', () => {
+    state.auth.user.level = 0
+    expect( createIsAdminSelector( user => user.level === 1)( state ) ).toEqual( false )
+  })
+
+})
